0c9bf6a75e7d89d2e74221f35878c18c329e6572
Tasinpronoy26 <tasinpronoy56@gmail.com>
2023-05-03 16:52:44 +0600

    Json file of chef

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const chef = require('./Data/chef.json');
 
 app.get("/" , (req, res) => {
 
     res.send('the kitchen')
+})
+
+app.get("/chef" , (req, res) => {
+
+    res.send(chef)
 })
 
 
