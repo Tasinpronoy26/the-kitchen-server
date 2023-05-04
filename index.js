d37b01c7c15cf222f8c57994659864451c602066
Tasinpronoy26 <tasinpronoy56@gmail.com>
2023-05-04 15:40:16 +0600

    Json File Fix

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ const singleChef = require('./Data/chef.json');
 
 app.use(cors());
 
-app.get("/" , (req, res) => {
+app.get("/", (req, res) => {
 
     res.send('the kitchen')
 })
 
-app.get("/chef" , (req, res) => {
+app.get("/chef", (req, res) => {
 
     res.send(chef)
 })
 
-app.get("/chef/:id" , (req, res) => {
+app.get("/chef/:id", (req, res) => {
 
     const id = req.params.id;
     console.log(id);
     const selectChef = singleChef.find(n => n.chefId == id);
     res.send(selectChef);
-    
+
 })
 
 
